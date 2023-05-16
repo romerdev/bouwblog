7a1b81198c0fe7d3be41ea4be69fa2feea44ead3
Romer <romer@nostal.dev>
2023-05-16 18:50:06 +0200

    feat(news): remove typo and change colors

--- app/page.tsx
+++ app/page.tsx
@@ -14,19 +14,19 @@ export default function Home() {
               height={450}
               priority
             />
-            <div className="md:absolute md:bottom-0 right-0 xl:translate-x-1/4 md:translate-y-1/4 md:h-80 md:w-80 md:bg-white pt-4 md:p-14 md:flex justify-center items-center md:rounded-full font-light text-lg">
+            <div className="md:absolute md:bottom-0 right-0 xl:translate-x-1/4 md:translate-y-1/4 md:h-80 md:w-80 md:bg-stone-100 pt-4 md:p-14 md:flex justify-center items-center md:rounded-full font-light text-lg">
               <p>In september start ik met de opleiding <strong>Built Environment</strong> op de Hogeschool van Amsterdam. Op deze website hou ik bij wat ik allemaal leer.</p>
             </div>
         </div>
 
         <ul className="grid md:grid-cols-2 xl:grid-cols-3 gap-12">
-          <li className="bg-stone-100 p-8 md:p-12 flex flex-col justify-end">
+          <li className="bg-amber-100 p-8 md:p-12 flex flex-col justify-end">
             <h2 className="text-light text-3xl md:text-4xl mb-4">It&apos;s official!</h2>
             <p className="md:text-lg">De inschrijving is rond! 🍾 In september 2023 kan ik officieel beginnen met de opleiding!</p>
           </li>
-          <li className="bg-purple-100 p-8 md:p-12">
+          <li className="bg-blue-100 p-8 md:p-12">
             <h2 className="text-light text-3xl md:text-4xl mb-4">Open dag bijgewoond</h2>
-            <p className="md:text-lg">Als onderdeel van de studiekeuzecheck moesten we in groepsverband werken aan een oplossing op het vraagstuk: &lsquo;Amsterdam moet in 2025 volledig autovrij zijn&rsquo;. </p>
+            <p className="md:text-lg">Als onderdeel van de studiekeuzecheck moesten we in groepsverband werken aan een oplossing op het vraagstuk: &lsquo;Amsterdam moet in 2050 volledig autovrij zijn&rsquo;. </p>
           </li>
         </ul>
       </Wrapper>
