a253be7aeeca50406769d92f5ff8d063f814d123
Romer <romer@nostal.dev>
2023-05-21 21:21:38 +0200

    fix(cards): make dates smaller on mobile

--- components/card.tsx
+++ components/card.tsx
@@ -12,7 +12,7 @@ const formattedDate: string =dayjs().to(dayjs(date));
     <div className={`${colors[0]} p-8 md:p-12 md:pt-14 flex flex-col justify-end h-full relative`}>
       <h2 className="text-light text-3xl md:text-4xl max-w-[15rem] mb-4">{title}</h2>
       <p className="md:text-lg">{children}</p>
-      <p className={`${colors[1]} w-fit text-sm pt-2 pb-1 px-4 rounded-full absolute top-5 right-4`}>{formattedDate}</p>
+      <p className={`${colors[1]} w-fit text-xs md:text-sm pt-2 pb-1 px-4 rounded-full absolute top-3 md:top-5 right-2 md:right-4`}>{formattedDate}</p>
     </div>
   );
 }
